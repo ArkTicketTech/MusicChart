9c737f8514a40d89f8344b7869d5622f3ef6e8f6
Yuanfanqin <1035211611@qq.com>
2016-04-04 17:58:20 +0800

    add:注入admin后台配置

--- backEnd/musicChart/users/models.py
+++ backEnd/musicChart/users/models.py
@@ -30,10 +30,52 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, username , password):
+        if not username:
+            raise ValueError('Users must have an username')
+
+        user = self.model(
+            username = username,
+        )
+
+        user.set_password(password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser):
     username = models.CharField(max_length=100,  unique=True, default="")
     # password = models.CharField(max_length=100, default="")
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
+
+    def get_full_name(self):
+        # The user is identified by their mobile
+        return self.username
+
+    def get_short_name(self):
+        # The user is identified by their mobile
+        return self.username
+
+    def __unicode__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
