cf395e6671c722866af9776df8255fdd1bde3bc4
Yuanfanqin <1035211611@qq.com>
2016-04-14 18:57:58 +0800

    fix:修改歌单admin视图

--- setlists/admin.py
+++ setlists/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Setlist,SetlistMusic
+from .models import Setlist
 
 # Register your models here.
-admin.site.register(Setlist)
-admin.site.register(SetlistMusic)
+class SetlistAdmin(admin.ModelAdmin):
+    filter_horizontal = ('musics',)
+admin.site.register(Setlist,SetlistAdmin)
